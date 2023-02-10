ad812450de8f0aaac23e3b29aac78775e6efdb34
NdekoCode <arickbulakali@gmail.com>
2023-02-10 09:37:19 +0200

    docs: positif number of multiplication

--- resolutions/03.conditions.php
+++ resolutions/03.conditions.php
@@ -1,7 +1,17 @@
 <?php
-$nbr = (float)readline("Entrer un nombre");
+// 01.
+/* $nbr = (float)readline("Entrer un nombre");
 if ($nbr > 0) {
     print "Le nombre est positif";
 } else {
     print "Le nombre est negatif";
+} */
+
+$nbr1 = (int)readline("Entrer un nombre\n");
+$nbr2 = (int)readline("Entrer un deuxieme nombre\n");
+
+if (($nbr1 < 0 && $nbr2 < 0) || ($nbr1 > 0 && $nbr2 > 0)) {
+    print "Le produit est positif\n";
+} else {
+    print "Le produit est négatif\n";
 }
