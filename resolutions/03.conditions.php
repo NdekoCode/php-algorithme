0dae0c50988cd03a16310992bc9b52a20bb42c05
NdekoCode <arickbulakali@gmail.com>
2023-02-10 11:50:03 +0200

    docs: verify alphatics name

--- resolutions/03.conditions.php
+++ resolutions/03.conditions.php
@@ -6,7 +6,7 @@ if ($nbr > 0) {
 } else {
     print "Le nombre est negatif";
 } */
-
+/* 
 $nbr1 = (int)readline("Entrer un nombre\n");
 $nbr2 = (int)readline("Entrer un deuxieme nombre\n");
 
@@ -14,4 +14,14 @@ if (($nbr1 < 0 && $nbr2 < 0) || ($nbr1 > 0 && $nbr2 > 0)) {
     print "Le produit est positif\n";
 } else {
     print "Le produit est négatif\n";
+}
+ */
+$nom1 = readline("Entrer un nom : \n");
+$nom2 = readline("Entrer un autre nom : \n");
+$nom3 = readline("Entrer un autre nom : \n");
+if ($nom1 < $nom2 && $nom2 < $nom3) {
+    echo "Ces noms sont classées de manière alphabetique\n";
+} else {
+
+    echo "Ces noms ne sont pas classées de manière alphabetique\n";
 }
