2c3be28bf29e64a0f41244d4ca05069c8202bccb
NdekoCode <arickbulakali@gmail.com>
2023-02-26 13:48:51 +0200

    docs: find bigger number and is position with while loop

--- resolutions/05/08.bigger_number_and_their_position.php
+++ resolutions/05/08.bigger_number_and_their_position.php
@@ -7,13 +7,16 @@
  * - Entrez le nombre numéro 20 : 6
  * - Le plus grand de ces nombres est : 14
  * Modifiez ensuite l’algorithme pour que le programme affiche de surcroît en quelle position avait été saisie ce nombre : C’était le nombre numéro 2 */
-$position = 0;
+$position = 1;
+$pos = 1;
 $biggerNumber = 0;
-for ($i = 1; $i <= 20; $i++) {
+$number = null;
+while ($number !== 0) {
     $number = (int) readline("Entrer un nombre : \n");
     if ($number > $biggerNumber) {
         $biggerNumber = $number;
-        $position = $i;
+        $pos  = $position;
     }
+    $position++;
 }
-print "Le nombre le plus grand est: $biggerNumber, il a été pris à la position $position\n";
+print "Le nombre le plus grand est: $biggerNumber, il a été pris à la position $pos\n";
