f29feb249246c44a046393746750cca904e5c497
NdekoCode <arickbulakali@gmail.com>
2023-02-27 00:27:58 +0200

    docs: sum of array items

--- resolutions/06/09.sum_of_array_item.php
+++ resolutions/06/09.sum_of_array_item.php
@@ -3,3 +3,9 @@
 /**
  * Ecrivez un algorithme calculant la somme des valeurs d’un tableau (on suppose que le tableau a été préalablement saisi).
  */
+$arr = [11, 14, 12, 11, 2, 8, 11, 10];
+$sum = 0;
+for ($i = 0; $i < count($arr); $i++) {
+    $sum += $arr[$i];
+}
+print "La somme des element du table est : $sum \n";
