2ad3e71d736dd16ffcdd817934d40610fd8a160f
NdekoCode <arickbulakali@gmail.com>
2023-02-26 23:21:20 +0200

    init array exercices

---
+++ resolutions/06/01.array.php
@@ -0,0 +1,8 @@
+<?php
+
+/** Ecrire un algorithme qui déclare et remplisse un tableau de 7 valeurs numériques en les mettant toutes à zéro. */
+$array = [];
+for ($i = 0; $i < 7; $i++) {
+    $array[] = 0;
+}
+print_r($array);
---
+++ resolutions/06/02.voyelles.php
@@ -0,0 +1,11 @@
+<?php
+
+/** Ecrire un algorithme qui déclare et remplisse un tableau contenant les six voyelles de l’alphabet latin. */
+$voyelles = [];
+$voyelles[] = "a";
+$voyelles[] = "e";
+$voyelles[] = "i";
+$voyelles[] = "o";
+$voyelles[] = "u";
+$voyelles[] = "y";
+print_r($voyelles);
