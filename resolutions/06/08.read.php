763f9bf837b351eb717b35c07c838667b656d391
NdekoCode <arickbulakali@gmail.com>
2023-02-27 00:18:54 +0200

    refactor: read negatif or positif number exoercice

--- resolutions/06/08.read.php
+++ resolutions/06/08.read.php
@@ -4,16 +4,16 @@
  * Ecrivez un algorithme permettant à l’utilisateur de saisir un nombre quelconque de valeurs, qui devront être stockées dans un tableau. L’utilisateur doit donc commencer par entrer le nombre de valeurs qu’il compte saisir. Il effectuera ensuite cette saisie. Enfin, une fois la saisie terminée, le programme affichera le nombre de valeurs négatives et le nombre de valeurs positives.
  */
 $number = (int) readline("Entrer le nombre de valeur que vous voulez entrer :\n");
-$positif = [];
-$negatif = [];
+$positif = 0;
+$negatif = 0;
 $numersUser = [];
 for ($i = 0; $i < $number; $i++) {
     $input = (int)readline("Entrer le nombre numero " . ($i + 1) . ": \n");
     $numersUser[] = $input;
     if ($input >= 0) {
-        $positif[] = $input;
+        $positif++;
     } else {
-        $negatif[] = $input;
+        $negatif++;
     }
 }
-print "Le nombre de valeurs négatives est " . (count($negatif)) . " et le nombre de valeurs positives " . (count($positif)) . "\n";
+print "Le nombre de valeurs négatives est $negatif et le nombre de valeurs positives $positif\n";
---
+++ resolutions/06/09.sum_of_array_item.php
@@ -0,0 +1,5 @@
+<?php
+
+/**
+ * Ecrivez un algorithme calculant la somme des valeurs d’un tableau (on suppose que le tableau a été préalablement saisi).
+ */
